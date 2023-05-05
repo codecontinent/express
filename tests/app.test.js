1e27b91cfa5eb1f4613c6d0d7d0f23b9615142f3
Mahabub Alam <mahabubx7@gmail.com>
2023-05-05 21:59:16 +0600

    :hammer: setup:tests

---
+++ tests/app.test.js
@@ -0,0 +1,24 @@
+import http from 'supertest';
+import app from '../src/app';
+
+describe('Boot the server', () => {
+  it('should spin the server', async () => {
+    const response = await http(app).get('/');
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('should return a HTTP:404 at /abc', async () => {
+    const response = await http(app).get('/abc');
+    expect(response.statusCode).toBe(404);
+  });
+
+  it('should return a HTTP:200 at /api', async () => {
+    const response = await http(app).get('/api');
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('should return a HTTP:404 at /api/abc', async () => {
+    const response = await http(app).get('/api/abc');
+    expect(response.statusCode).toBe(404);
+  });
+});
